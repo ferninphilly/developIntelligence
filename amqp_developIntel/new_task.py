6715a3994b584741910680712ec3050e266f1e8b
fpombeiro@fandango.com <fpombeiro@fandango.com>
2016-10-31 17:14:06 -0700

    Balah

--- amqp_developIntel/new_task.py
+++ amqp_developIntel/new_task.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
+message = ' '.join(sys.argv[1:]) or "Rogue One"
 channel.basic_publish(exchange='',
                       routing_key='task_queue',
                       body=message,
--- amqp_developIntel/send.py
+++ amqp_developIntel/send.py
@@ -5,10 +5,10 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='mailbox')
 
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+                      routing_key='mailbox',
+                      body='The Times They are a changin')
+print(" [x] Sent 'The Times They are a changin'")
 connection.close()
